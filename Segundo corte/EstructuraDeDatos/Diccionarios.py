3da6b40f6e6e38deab3f1dc86d01cf55f4fadea2
Nicolas <142908207+Nicholas8O4@users.noreply.github.com>
2023-11-13 10:41:53 -0500

    Update Diccionarios.py

--- Segundo corte/EstructuraDeDatos/Diccionarios.py
+++ Segundo corte/EstructuraDeDatos/Diccionarios.py
@@ -56,4 +56,44 @@ zipbebidas = zip(bebidas,Azucares)
 
 bebidasYazucares = {key:value for key, value in zipbebidas}
 print(bebidasYazucares)
+print()
+print()
 
+
+# Listas de nombres y alturas en pulgadas
+nombres = ['Jenny', 'Alexus', 'Sam', 'Grace']
+alturas = [61, 70, 67, 64]
+
+# Se crea un diccionario que relaciona nombres con alturas usando comprensión de diccionarios
+estudiantes = {llave: valor for llave, valor in zip(nombres, alturas)}
+# estudiantes es ahora {'Jenny': 61, 'Alexus': 70, 'Sam': 67, 'Grace': 64}
+
+# Listas de bebidas y contenido de cafeína
+bebidas = ["espresso", "chai", "decaf", "drip"]
+contenido_cafeina = [64, 40, 0, 120]
+
+# Se utiliza zip() para combinar las listas en un iterador de tuplas con elementos de las listas emparejados
+bebidas_a_contenido = {llave: valor for llave, valor in zip(bebidas, contenido_cafeina)}
+
+# Listas de canciones y número de reproducciones
+canciones = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
+repeticiones = [78, 29, 44, 21, 89, 5]
+
+print()
+
+# Se crea un diccionario que relaciona canciones con el número de reproducciones
+reproducciones = {llave: valor for llave, valor in zip(canciones, repeticiones)}
+print(reproducciones)  # Muestra el diccionario de canciones y reproducciones
+
+print()
+
+# Se actualizan las reproducciones de dos canciones existentes y se añade una nueva
+reproducciones.update({"Purple Haze": 1})
+reproducciones.update({"Respect": 94})
+print("Después: ", reproducciones)  # Muestra el diccionario actualizado de reproducciones
+
+print()
+
+# Se crea un diccionario llamado 'biblioteca' que contiene los diccionarios de reproducciones y otro vacío
+biblioteca = {"Las Mejores Canciones": reproducciones, "Sentimientos de Domingo": {}}
+print(biblioteca)  # Muestra el contenido de la biblioteca
